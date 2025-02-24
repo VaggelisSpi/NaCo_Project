7d7ab171935ba50aec4d19af78a8ab63b2ba3bb4
vaggelis.spithas <vaggelisspithas@gmail.com>
2025-02-24 12:03:55 +0100

    Add preprocessing steps

--- naco_proj/assignment2/syscall_classification.py
+++ naco_proj/assignment2/syscall_classification.py
@@ -14,6 +14,9 @@
 #     name: python3
 # ---
 
+# %% [markdown]
+# # Imports and setup
+
 # %%
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -21,6 +24,9 @@ import matplotlib.pyplot as plt
 # %%
 snd_cert_path = './data/syscalls/snd-cert'
 snd_unm_path = './data/syscalls/snd-unm'
+
+# %% [markdown]
+# ## Load the data
 
 # %%
 snd_cert_1_data = pd.DataFrame()
@@ -56,10 +62,18 @@ snd_unm_train_data = pd.DataFrame()
 snd_unm_train_data["data"] = pd.read_csv(snd_unm_path + "/snd-unm.train")
 
 
+# %% [markdown]
+# # Preprocessing
+
 # %%
-def preprocess_data_frames(df: pd.DataFrame) -> pd.DataFrame:
+def preprocess_data_frame(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Perform any preprocessing steps in a single data frame
+
+    We assign a new id column and a new column with the length of the data
+    """
     # assign a unique id to each data element so when we split them in substrings, we can then get back to the original
-    # and coclude about the final class
+    # and conclude about the final class
     df['id'] = range(len(df))
 
     df["length"] = df["data"].str.len()
@@ -68,31 +82,49 @@ def preprocess_data_frames(df: pd.DataFrame) -> pd.DataFrame:
 
 def extract_substrings(df: pd.DataFrame, substr_len: int = 7) -> pd.DataFrame:
     """
-    Create a new dataframe with all the subsrings from each row
+    Create a new dataframe with all the substrings from each row
     We will try to get non overlapping substrings, but if the text is not divisible by the desired length, then for the
     last substring we will get the last substr_len elements of the string
 
     This method returns a dataframe with the substrings and the label of the original string
     """
-    substr_df = {"data": [], "label": []}
+    # note for improvement, check the apply method of data frame paired with explode
+    substr_df = {"data": [], "label": [], "id": []}
     for index, row in df.iterrows():
         text = row["data"]
         label = row["label"]
+        id = row["id"]
         substrings = []
-        start = 0
         length = row["length"]
+        start = 0
         while start < length:
             end = start + substr_len
             if end < length:
                 substrings.append(text[start:end])
             else:
-                substrings.append(text[-length:])
-        substr_df["data"].extend(substrings)
-        # substr_df["label"].extend(label for i in range(len(substrings)))
+                substrings.append(text[-substr_len:])
+            start = end
 
-    print(substr_df["data"])
-    # return pd.DataFrame(substr_df)
-    return pd.DataFram
+        substr_df["data"].extend(substrings)
+        substr_df["label"].extend(label for i in range(len(substrings)))
+        substr_df["id"].extend(id for i in range(len(substrings)))
+
+    return pd.DataFrame(substr_df)
+
+def preprocess(df: pd.DataFrame, name: str, data_path: str, is_train: bool = False) -> None:
+    """
+    Apply all the preprocessing steps in a dataframe and save all the resulting dataframes
+    """
+    df = preprocess_data_frame(df)
+    df.to_csv(data_path + "/" + name + ".csv")
+
+    # extract substrings and save the new dataframe
+    df_substr = extract_substrings(df)
+    df_substr.to_csv(data_path + "/" + name + "_substr.csv")
+
+    # save the data in a file to be used in the negative selection algorithm
+    df_substr["data"].to_csv(data_path + "/" + name + "_substr" + ".train" if is_train else ".test", header=False, index=False)
+
 
 
 # %%
@@ -101,38 +133,16 @@ def analyse_df(df: pd.DataFrame, name: str) -> None:
     display(df.groupby("length").count())
 
 
-# %%
-snd_cert_1_data = preprocess_data_frames(snd_cert_1_data)
-snd_cert_2_data = preprocess_data_frames(snd_cert_2_data)
-snd_cert_3_data = preprocess_data_frames(snd_cert_3_data)
-snd_cert_train_data = preprocess_data_frames(snd_cert_train_data)
-
-snd_unm_1_data = preprocess_data_frames(snd_unm_1_data)
-snd_unm_2_data = preprocess_data_frames(snd_unm_2_data)
-snd_unm_3_data = preprocess_data_frames(snd_unm_3_data)
-snd_unm_train_data = preprocess_data_frames(snd_unm_train_data)
-
-# analyse_df(snd_cert_1_data, "snd_cert_1_data")
-# analyse_df(snd_cert_2_data, "snd_cert_2_data")
-# analyse_df(snd_cert_3_data, "snd_cert_3_data")
-# analyse_df(snd_cert_train_data, "snd_cert_train_data")
-
-# analyse_df(snd_unm_1_data, "snd_unm_1_data")
-# analyse_df(snd_unm_2_data, "snd_unm_2_data")
-# analyse_df(snd_unm_3_data, "snd_unm_3_data")
-# analyse_df(snd_unm_train_data, "snd_unm_train_data")
+# %% [markdown]
+# Preprocess and save all the data
 
 # %%
-# snd_cert_1_substrs_data = extract_substrings(snd_cert_1_data)
-# snd_cert_1_substrs_data.head(20)
-# this is too slow currently
+preprocess(snd_cert_1_data, "snd_cert_1", snd_cert_path)
+preprocess(snd_cert_2_data, "snd_cert_2", snd_cert_path)
+preprocess(snd_cert_3_data, "snd_cert_3", snd_cert_path)
+preprocess(snd_cert_train_data, "snd_cert_train", snd_cert_path, True)
 
-# %%
-
-snd_cert_1_data.to_csv(snd_cert_path + "/snd_cert.1.csv")
-snd_cert_2_data.to_csv(snd_cert_path + "/snd_cert.2.csv")
-snd_cert_3_data.to_csv(snd_cert_path + "/snd_cert.3.csv")
-
-snd_unm_1_data.to_csv(snd_unm_path + "/snd_unm.1.csv")
-snd_unm_2_data.to_csv(snd_unm_path + "/snd_unm.2.csv")
-snd_unm_3_data.to_csv(snd_unm_path + "/snd_unm.3.csv")
+preprocess(snd_unm_1_data, "snd_unm_1", snd_unm_path)
+preprocess(snd_unm_2_data, "snd_unm_2", snd_unm_path)
+preprocess(snd_unm_3_data, "snd_unm_3", snd_unm_path)
+preprocess(snd_unm_train_data, "snd_unm_train", snd_unm_path, True)
