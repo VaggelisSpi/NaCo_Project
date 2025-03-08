e3abcd5264f8b963e60c309999134b8481e8c90e
dewi-elisa <dewi.timman@gmail.com>
2025-03-08 19:23:15 +0100

    3.1 so far

--- assignment3/si-exercises/exercise_pso/exercise3.1.py
+++ assignment3/si-exercises/exercise_pso/exercise3.1.py
@@ -29,11 +29,56 @@ def color_quantization(image, colors):
         # assign the nearest color to the pixel
         pixels[i] = best_color
 
-    return Image.fromarray(pixels.reshape(image.shape))
+    # make an image again
+    new_image = Image.fromarray(pixels.reshape(image.shape))
+
+    # export image
+    path = "./assignment3/si-exercises/exercise_pso/"
+    new_image.save(path + 'quantized_imgage.png')
 
 
-def PSO():
-    pass
+def PSO(image, n, evaluation, omega, alpha1, alpha2):
+    # get n random pixel positions
+    height, width = image.size
+    x_coords = np.randint(0, width, n)
+    y_coords = np.randint(0, height, n)
+
+    x = np.array(zip(x_coords, y_coords))
+
+    # initialize velocities, local best and global best
+    velocity = np.zeros(n)
+    local_best = np.zeros(n)
+    global_best = 0
+    happy = False
+
+    # write initial state to file
+    # TODO
+
+    # execute PSO algorithm
+    while not happy:
+        for i in range(n):
+            r1 = 0  # TODO
+            r2 = 0  # TODO
+            velocity = omega * velocity + alpha1 * r1 * (local_best - x) + alpha2 * r2 * (global_best - x)
+        for i in range(n):
+            x = x + velocity
+            if evaluation(x[i]) < evaluation(x[i]):  # TODO
+                local_best[i] = x[i]
+            if evaluation(x[i]) < evaluation(global_best):
+                global_best = x[i]
+        # write state to file
+        # TODO
+
+        # At several iterations of your PSO algorithm, show both the best color palette found so far, and the image quantized with this palette.
+        # TODO
+
+
+# thoughts (not sure if they are right and my most recent thoughts)
+# points: colored pixels
+# clusters: colors that are used most
+# agents: pixels
+# fitness: how often a color is used, or, distance
+# particle: the best color to choose from
 
 
 def k_means():
@@ -41,6 +86,7 @@ def k_means():
 
 
 if __name__ == "__main__":
+    # QUANTIZATION
     # import image
     path = "./assignment3/si-exercises/exercise_pso/"
     img = Image.open(path + 'image.png')
@@ -53,8 +99,9 @@ if __name__ == "__main__":
     colors = np.array([light_green, dark_red, light_red, dark_green])
 
     # color quantization
-    quantized_img = color_quantization(img, colors)
-    quantized_img.show()
+    color_quantization(img, colors)
 
-    # export image
-    quantized_img.save(path + 'quantized_imgage.png')
+    # PSO
+    PSO(img, 4, color_distance, 0.73, 1.5, 1.5)  # parameter omega, alpha1, alpha2 from lecture
+
+    # K-MEANS
