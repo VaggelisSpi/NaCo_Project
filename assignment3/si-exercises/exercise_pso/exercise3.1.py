289bd6072271af9386c2bbf742359d627f182524
dewi-elisa <dewi.timman@gmail.com>
2025-03-08 22:50:08 +0100

    PSO?

--- assignment3/si-exercises/exercise_pso/exercise3.1.py
+++ assignment3/si-exercises/exercise_pso/exercise3.1.py
@@ -2,6 +2,19 @@ from PIL import Image
 import numpy as np
 
 
+def nearest_color(pixel, colors):
+    best_color = None
+    best_distance = np.inf
+
+    for color in colors:
+        distance = color_distance(pixel, color)
+        if distance < best_distance:
+            best_distance = distance
+            best_color = color
+
+    return best_color, best_distance
+
+
 def color_distance(col1, col2):
     # for now I implemented Euclidean distance but if we want we can optimize this function to
     # be more perceptually accurate: https://en.wikipedia.org/wiki/Color_difference
@@ -10,75 +23,81 @@ def color_distance(col1, col2):
                    (col1[2] - col2[2]) ** 2)
 
 
-def color_quantization(image, colors):
+def color_quantization(image, colors, name):
     image = np.array(image, dtype=np.uint8)
     pixels = image.reshape(-1, 3)
 
     # change every pixel to the nearest color
     for i, pixel in enumerate(pixels):
-        best_color = None
-        best_distance = np.inf
-
-        # check which color is the nearest color
-        for color in colors:
-            distance = color_distance(pixel, color)
-            if distance < best_distance:
-                best_distance = distance
-                best_color = color
-
         # assign the nearest color to the pixel
-        pixels[i] = best_color
+        pixels[i], _ = nearest_color(pixel, colors)
 
     # make an image again
     new_image = Image.fromarray(pixels.reshape(image.shape))
 
     # export image
     path = "./assignment3/si-exercises/exercise_pso/"
-    new_image.save(path + 'quantized_imgage.png')
+    new_image.save(path + name)
 
 
-def PSO(image, n, evaluation, omega, alpha1, alpha2):
-    # get n random pixel positions
-    height, width = image.size
-    x_coords = np.randint(0, width, n)
-    y_coords = np.randint(0, height, n)
+def PSO(image, k, n_particles, evaluation=nearest_color, max_iter=50, omega=0.73, alpha1=1.5, alpha2=1.5):
+    image = np.array(image, dtype=np.uint8)
+    pixels = image.reshape(-1, 3)
 
-    x = np.array(zip(x_coords, y_coords))
+    # get k random colors for each particle
+    x = np.random.randint(0, 256, (n_particles, k, 3))
 
     # initialize velocities, local best and global best
-    velocity = np.zeros(n)
-    local_best = np.zeros(n)
-    global_best = 0
-    happy = False
+    velocity = np.zeros_like(x)
+    local_best = np.copy(x)
+    local_best_score = np.full(n_particles, np.inf)
+    global_best = np.copy(x)
+    global_best_score = np.inf
+    iter = 0
 
     # write initial state to file
-    # TODO
+    path = "./assignment3/si-exercises/exercise_pso/"
+    f = open(path + "PSO_results.txt", "a")
+    f.write("Iteration: " + str(iter) + "\n")
+    f.write("x: " + str(x) + "\n")
+    f.write("velocity: " + str(velocity) + "\n")
+    f.write("local best: " + str(local_best) + "\n")
+    f.write("global_best: " + str(global_best) + "\n\n")
+    f.close()
 
     # execute PSO algorithm
-    while not happy:
-        for i in range(n):
-            r1 = 0  # TODO
-            r2 = 0  # TODO
+    while iter < max_iter:
+        iter += 1
+        for i in range(n_particles):
+            r1 = np.random.rand(3)
+            r2 = np.random.rand(3)
             velocity = omega * velocity + alpha1 * r1 * (local_best - x) + alpha2 * r2 * (global_best - x)
-        for i in range(n):
-            x = x + velocity
-            if evaluation(x[i]) < evaluation(x[i]):  # TODO
+        x = np.array(x + velocity, dtype=int)
+        for i in range(n_particles):
+            score = 0
+            for pixel in pixels:
+                score += evaluation(pixel, x[i])[1]
+            if score < local_best_score[i]:
                 local_best[i] = x[i]
-            if evaluation(x[i]) < evaluation(global_best):
+                local_best_score[i] = score
+            if score < global_best_score:
                 global_best = x[i]
+                global_best_score = score
+
         # write state to file
-        # TODO
+        f = open(path + "PSO_results.txt", "a")
+        f.write("Iteration: " + str(iter) + "\n")
+        f.write("x: " + str(x) + "\n")
+        f.write("velocity: " + str(velocity) + "\n")
+        f.write("local best: " + str(local_best) + "\n")
+        f.write("global_best: " + str(global_best) + "\n\n")
+        f.close()
 
-        # At several iterations of your PSO algorithm, show both the best color palette found so far, and the image quantized with this palette.
-        # TODO
-
-
-# thoughts (not sure if they are right and my most recent thoughts)
-# points: colored pixels
-# clusters: colors that are used most
-# agents: pixels
-# fitness: how often a color is used, or, distance
-# particle: the best color to choose from
+        # show color palette so far + image every 5 iterations
+        if iter % 5 == 0:
+            print(global_best)
+            name = "PSO" + str(iter) + ".png"
+            color_quantization(image, global_best, name)
 
 
 def k_means():
@@ -99,9 +118,9 @@ if __name__ == "__main__":
     colors = np.array([light_green, dark_red, light_red, dark_green])
 
     # color quantization
-    color_quantization(img, colors)
+    color_quantization(img, colors, 'quantized_image.png')
 
     # PSO
-    PSO(img, 4, color_distance, 0.73, 1.5, 1.5)  # parameter omega, alpha1, alpha2 from lecture
+    PSO(img, 4, 4, nearest_color)  # parameter omega, alpha1, alpha2 from lecture
 
     # K-MEANS
