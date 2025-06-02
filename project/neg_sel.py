d55c020985a3b75b7ff7abc44cf80eb1ed73336e
vaggelis.spithas <vaggelisspithas@gmail.com>
2025-06-02 12:27:33 +0200

    Calculate ROC and AUC as a metric to evaluate the negative selection algorithm

--- project/neg_sel.py
+++ project/neg_sel.py
@@ -1,4 +1,6 @@
 import subprocess
+import pandas as pd
+import numpy as np
 
 
 class NegativeSelection:
@@ -30,3 +32,21 @@ class NegativeSelection:
                 "> ./data/results/" + str(data_path.split("/")[-1][:-4]) + "_r" + str(r) + ".txt"
             )
             subprocess.run(cmd, capture_output=True, shell=True)
+
+
+def load_data(data_path: str, r: int, anomalous: int = 0) -> pd.DataFrame:
+    """
+    Load the strings and their scores in a dataframe along with the label
+
+    There are two files, one that the data_path parameter points to that contains the strings, and the one under the 
+    results directory with the scores for the data in data_path. For each different r value we trained the negative 
+    selection algorithm, there will be a different score file.
+    Finally the dataframe will contain a column called anomalous, that will indicate the class of the data. This is 
+    determined from the parameter of the same name
+    """
+    data = pd.DataFrame()
+    data["input"] = pd.read_csv(data_path, header=None)  # input sentence
+    data["score"] = pd.read_csv("./data/results/" + str(data_path.split("/")[-1][:-4]) + "_r" + str(r) + ".txt", header=None).astype(np.float32)  # anomaly score
+    data["anomalous"] = anomalous
+
+    return data
