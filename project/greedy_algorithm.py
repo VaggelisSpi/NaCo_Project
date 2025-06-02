6dc0bafb81543ec908327b66c0115a9ddedc3de3
vaggelis.spithas <vaggelisspithas@gmail.com>
2025-06-02 12:28:03 +0200

    Fix style

--- project/greedy_algorithm.py
+++ project/greedy_algorithm.py
@@ -5,40 +5,41 @@ import random
 from typing import List
 import math
 
+
 class GreedyAlgorithm:
-    '''A greedy optimizer implementation based on the paper 'Is T Cell Negative Selection a Learning Algorithm?' 
+    """A greedy optimizer implementation based on the paper 'Is T Cell Negative Selection a Learning Algorithm?'
     (Wortel et al., 2020).
-    '''
+    """
+
     def __init__(
         self,
-        peptides: List[str], # we assume that each peptide is of same length l
-        motifs: List[str], # we also assume that the motifs are of same length l
-        t: int, # threshold
+        peptides: List[str],  # we assume that each peptide is of same length l
+        motifs: List[str],  # we also assume that the motifs are of same length l
+        t: int,  # threshold
         seed: int | None = None,
     ):
         self.peptides = peptides
         self.motifs = motifs
         self.t = t
-        self.l = len(self.peptides[0]) # peptide length
-        
+        self.l = len(self.peptides[0])  # peptide length
+
         if seed is not None:
             random.seed(seed)
             np.random.seed(seed)
-    
+
     def run(self):
         remaining_motifs = set(self.motifs)
         reactions_map = self._self_reactive_pairs()
         selected_peptides = []
 
         while remaining_motifs:
-            
             # calculate for each peptide the number of reactions with the remaining self-reactive TCRs
             peptide_deletion_counts = {}
             for peptide in self.peptides:
                 # calculate all reactions the peptide has on the remaining motifs set
-                deletable = reactions_map[peptide] & remaining_motifs # TODO improve speed?
+                deletable = reactions_map[peptide] & remaining_motifs  # TODO improve speed?
                 peptide_deletion_counts[peptide] = len(deletable)
-            
+
             # find the self peptide that deletes the most of these remaining self-reactive TCRs
             max_deleted = max(peptide_deletion_counts.values())
             if max_deleted == 0:
@@ -48,8 +49,8 @@ class GreedyAlgorithm:
             for peptide, count in peptide_deletion_counts.items():
                 if count == max_deleted:
                     top_peptides.append(peptide)
-            
-            # if multiple self peptides delete an equal number of remaining TCRs, we pick only those self peptides that 
+
+            # if multiple self peptides delete an equal number of remaining TCRs, we pick only those self peptides that
             # do not overlap in the TCRs they delete
             deleted_this_round = set()
             chosen_peptides = []
@@ -61,13 +62,13 @@ class GreedyAlgorithm:
 
             # actually add the peptides to the list
             selected_peptides.extend(chosen_peptides)
-            
+
             # remove all deleted motifs from the set of remaining motifs
             for peptide in chosen_peptides:
                 remaining_motifs -= reactions_map[peptide]
 
         return selected_peptides
-        
+
     def _affinity(self, motif: str, peptide: str) -> int:
         max_adjacent = 0
         current = 0
@@ -79,7 +80,7 @@ class GreedyAlgorithm:
             else:
                 current = 0
         return max_adjacent
-    
+
     def _self_reactive_pairs(self):
         motif_peptide_map = {}
         for peptide in self.peptides:
--- project/neg_sel.py
+++ project/neg_sel.py
@@ -26,10 +26,21 @@ class NegativeSelection:
         """
         for r in range(self.r_start, self.r_stop):
             cmd = (
-                "java -jar negsel2.jar -alphabet " + self.alphabet_path +
-                " -self " + self.self_path + " -n " + str(self.n) + " -l -c -r "
-                + str(r) + " <" + data_path +
-                "> ./data/results/" + str(data_path.split("/")[-1][:-4]) + "_r" + str(r) + ".txt"
+                "java -jar negsel2.jar -alphabet "
+                + self.alphabet_path
+                + " -self "
+                + self.self_path
+                + " -n "
+                + str(self.n)
+                + " -l -c -r "
+                + str(r)
+                + " <"
+                + data_path
+                + "> ./data/results/"
+                + str(data_path.split("/")[-1][:-4])
+                + "_r"
+                + str(r)
+                + ".txt"
             )
             subprocess.run(cmd, capture_output=True, shell=True)
 
@@ -38,15 +49,17 @@ def load_data(data_path: str, r: int, anomalous: int = 0) -> pd.DataFrame:
     """
     Load the strings and their scores in a dataframe along with the label
 
-    There are two files, one that the data_path parameter points to that contains the strings, and the one under the 
-    results directory with the scores for the data in data_path. For each different r value we trained the negative 
+    There are two files, one that the data_path parameter points to that contains the strings, and the one under the
+    results directory with the scores for the data in data_path. For each different r value we trained the negative
     selection algorithm, there will be a different score file.
-    Finally the dataframe will contain a column called anomalous, that will indicate the class of the data. This is 
+    Finally the dataframe will contain a column called anomalous, that will indicate the class of the data. This is
     determined from the parameter of the same name
     """
     data = pd.DataFrame()
     data["input"] = pd.read_csv(data_path, header=None)  # input sentence
-    data["score"] = pd.read_csv("./data/results/" + str(data_path.split("/")[-1][:-4]) + "_r" + str(r) + ".txt", header=None).astype(np.float32)  # anomaly score
+    data["score"] = pd.read_csv(
+        "./data/results/" + str(data_path.split("/")[-1][:-4]) + "_r" + str(r) + ".txt", header=None
+    ).astype(np.float32)  # anomaly score
     data["anomalous"] = anomalous
 
     return data
